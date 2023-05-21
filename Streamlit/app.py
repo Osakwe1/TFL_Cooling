f2e1bd025f18838663f9884590f22805e3b635db
Olisa Osakwe <olisaosakwe@gmail.com>
2023-05-21 14:13:56 +0100

    Streamlit & requirements added

---
+++ Streamlit/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(layout="wide", page_icon=":lower_left_paintbrush:", menu_items={
+    'Report a bug': "mailto:OOsakwe1@icloud.com?subject=Bug%20found%20in%20Outpainting",
+    'About': "# Built by Krishini, Louis, Hafiz & Olisa.",
+    'Get Help': "https://github.com/Osakwe1/Outpainting"
